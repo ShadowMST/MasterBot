63379f9147134547ea5464ca4d18daeda0618e90
MST <106149547+ShadowMST@users.noreply.github.com>
2022-07-21 09:27:41 +0000

    Updated with Kick, Ban, Unban and Clear Commands

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
+
 
 bot = commands.Bot(command_prefix="!")
 
-#Bot Commands
+#----------------------BOT COMMANDS------------------------------------------------------------
+
+
+#This code will clear messages!
 @bot.command()
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount) #Will clear messages from the Discord Channel
 
+
+#This code will reply when command error!
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
@@ -17,12 +22,23 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You dont have all the requirements :angry:")
 
+      
+#The below code Kicks People!
+@bot.command(aliases=['k'])
+@commands.has_permissions(kick_members = True)
+async def kick(ctx,member : discord.Member,*,reason= "No Reason Provided"):
+  await member.send("You have been kicked from the Community, Because:"+reason)
+  await member.kick(reason=reason)
+
+      
 #The below code bans player.
 @bot.command()
 @commands.has_permissions(ban_members = True)
-async def ban(ctx, member : discord.Member, *, reason = None):
+async def ban(ctx, member : discord.Member, *, reason = "No reason Provided"):
     await member.ban(reason = reason)
+    await member.send(member.name + "has been banned from the Community, because:"+reason)
 
+  
 #The below code unbans player.
 @bot.command()
 @commands.has_permissions(administrator = True)
@@ -37,6 +53,7 @@ async def unban(ctx, *, member):
             await ctx.guild.unban(user)
             await ctx.send(f'Unbanned {user.mention}')
             return
+
 
 
 
