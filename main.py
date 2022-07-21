c93503ae8fdc1bc5a00c09ed8678124ef0eea233
MST <106149547+ShadowMST@users.noreply.github.com>
2022-07-21 05:20:56 +0000

    Added ban command along with other

--- main.py
+++ main.py
@@ -5,19 +5,38 @@ import asyncio
 
 bot = commands.Bot(command_prefix="!")
 
-@bot.command()
-async def hello(ctx):
-  await ctx.send("Hello!")
-
+#Bot Commands
 @bot.command()
 async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
+    await ctx.channel.purge(limit=amount) #Will clear messages from the Discord Channel
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please pass in all requirements :rolling_eyes:.')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have all the requirements :angry:")
+
+#The below code bans player.
 @bot.command()
-async def 
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    await member.ban(reason = reason)
 
+#The below code unbans player.
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
 
+    for ban_entry in banned_users:
+        user = ban_entry.user
 
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 
 
