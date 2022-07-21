68e3be13af731cdee2a95afecc5f2f11b78949d8
MST <106149547+ShadowMST@users.noreply.github.com>
2022-07-21 05:36:58 +0000

    Changed back to original

--- events/on_command_error.py
+++
@@ -1,6 +0,0 @@
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please pass in all requirements :rolling_eyes:.')
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You dont have all the requirements :angry:")
